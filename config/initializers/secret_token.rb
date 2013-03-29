d6afba3ce519c5b93beaee26aaf3296fd0a6db41
ashley williams <heyashleyashley@gmail.com>
2013-03-29 18:39:07 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetaNyc::Application.config.secret_token = '1c9c74ec1b0d7d017c2c1cedb846ea340df6389f2e9d82d59b5b953b7d61370e096ef26a284e6d2ea3741394af31c1c5ba215ccb50dfab90beb4872d55208ec5'
