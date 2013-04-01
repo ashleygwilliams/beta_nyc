69bf69e1937b6dba613105ec77da08fa3bf349ea
ashley williams <heyashleyashley@gmail.com>
2013-04-01 17:23:47 -0400

    add secure password

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130401195913) do
+ActiveRecord::Schema.define(:version => 20130401212108) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.string   "githandle"
     t.string   "cellphone"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
