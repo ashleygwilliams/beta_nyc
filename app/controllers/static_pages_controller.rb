ded1aff0449c9669a801522f20446a32a5ede6db
ashley williams <heyashleyashley@gmail.com>
2013-04-04 11:56:42 -0400

    add most recent event and project feeds to home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
-    @project = current_user.projects.build if signed_in?
+    @projects = Project.last(:limit => 3)
+    @events = Event.last(:limit => 3)
   end
 
   def about
