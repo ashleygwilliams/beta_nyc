a059bd6c962284490409589a0eecef172398255f
ashley williams <heyashleyashley@gmail.com>
2013-04-04 11:43:42 -0400

    fix typo in migration

--- db/migrate/20130402170831_create_projects.rb
+++ db/migrate/20130402170831_create_projects.rb
@@ -8,6 +8,6 @@ class CreateProjects < ActiveRecord::Migration
 
       t.timestamps
     end
-    add_index :microposts, [:user_id, :created_at]
+    add_index :projects, [:user_id, :created_at]
   end
 end
